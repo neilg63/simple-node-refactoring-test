b158b3ba3a6e1a6fbc364640f5056391ec80b237
Neil Gardner <neilgardner1963@gmail.com>
2026-01-07 23:27:44 +0000

    removed calcPercentage() implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as appInterface from './app.interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): appInterface.AppInfo {
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { AppInfo } from './app.interface';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): AppInfo {
+    return { message: 'Welcome to the Survey API', uri: '/survey/results' };
   }
 }
